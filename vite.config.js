6c16e0c52f0be1a1abb846cb40ace11f36eb0036
Abdul Quddos <abdulquddoshafiz@gmail.com>
2024-07-17 22:55:38 +0500

    applying css module strategy

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      globalModulePaths: ['src/components/Non-reusable Components/Dashboard Overview Card/Index.module.css'],
-    },
-  },
+
 })
   
